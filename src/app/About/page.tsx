411855a9f40a02c36e3b8b9ec3cbef0e4a3689d2
Hassan Omar <hassanomar7674@gmail.com>
2025-10-23 11:56:46 +0500

    f

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -3,7 +3,7 @@
 
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
-import bg from "../../Assests/bg.png";
+import bg from "../../Assests/bg.jpg";
 import about from "../../Assests/a.jpg";
 import Footer from "../Components/Footer"
 import Navbar from "../Components/Navbar";
