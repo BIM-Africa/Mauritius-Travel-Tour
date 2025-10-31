9d1581091640b818da43669555c63f8cbd1979f0
BIM-Africa <deployment@bim.africa>
2025-10-31 10:03:18 +0400

    Update page.tsx

--- src/app/WestTour/page.tsx
+++ src/app/WestTour/page.tsx
@@ -179,7 +179,7 @@ export default function HomePage() {
               icon: timing,
               text: (
                 <>
-                  Timing - <br />8 hrs
+                  Approximate Timing - 8 hrs
                 </>
               ),
             },
